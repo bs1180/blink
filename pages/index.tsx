7fb2b35d806189b4772c688d2dd9554b25823a4c
Ben Smith <ben.smith@tourradar.com>
2020-01-28 13:50:02 +0100

    Add facebook link

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,8 @@ const IndexPage: NextPage = () => {
     <div
       className="bg-blue-200 min-h-screen flex md:items-center justify-center"
       style={{
-        backgroundImage: "linear-gradient( 109.6deg,  rgba(31,179,237,1) 11.2%, rgba(17,106,197,1) 91.1% )"
+        backgroundImage:
+          "linear-gradient( 109.6deg,  rgba(31,179,237,1) 11.2%, rgba(17,106,197,1) 91.1% )"
       }}
     >
       <Head>
@@ -21,22 +22,43 @@ const IndexPage: NextPage = () => {
           className="bg-white border-solid border-black border-4 flex flex-col justify-content items-center mx-4"
           style={{ boxShadow: "10px 10px 0px 0px rgba(0,0,0,1)" }}
         >
-          <Logo className="w-64 -mt-24" width="300px" height="150px" style={{ transform: "translateY(0px)" }} />
+          <Logo
+            className="w-64 -mt-24"
+            width="300px"
+            height="150px"
+            style={{ transform: "translateY(0px)" }}
+          />
 
           <div className="p-4 sm:p-8 md:p-16 md:pt-8 flex flex-col justify-center items-center relative overflow-hidden">
             <div className="mb-8 z-20">
               <p>Dear improvisers!</p>
               <p className="pt-6">
-                We're organising an exciting new improv festival to be held <strong>12 – 14 June 2020</strong> at
-                the{" "}
-                <a href="https://spektakel.wien/" className="font-bold hover:underline">
+                We're organising an exciting new improv festival to be held{" "}
+                <strong>12 – 14 June 2020</strong> at the{" "}
+                <a
+                  href="https://spektakel.wien/"
+                  className="font-bold hover:underline"
+                >
                   Spektakel Theatre
                 </a>{" "}
-                in Vienna, Austria. There will be training, jam sessions and performances in both English and
-                German.
+                in Vienna, Austria. There will be whole days filled with improv
+                training, jam sessions and performances in both English and
+                German...basically just a weekend-long impro-party!
               </p>
-              <p className="pt-6">To stay informed please join our mailing list.</p>
-              <p className="pt-4 text-right text-gray-500">-- Christof, Hanni, Shawn, Florian, Coline and Ben</p>
+              <p className="pt-6">
+                To stay informed please join our mailing list below or follow us
+                on{" "}
+                <a
+                  className="underline hover:underline"
+                  href="https://www.facebook.com/BLINK.Improfestival"
+                >
+                  Facebook
+                </a>
+                .
+              </p>
+              <p className="pt-4 text-right text-gray-500">
+                -- Christof, Hanni, Shawn, Florian, Coline and Ben
+              </p>
             </div>
 
             <SignupForm />
